23b487a68fdbf7edded62da5d432a112614e228e
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2024-09-17 23:22:48 +0530

    python_practise_02

---
+++ Python_Practise/two.py
@@ -0,0 +1,12 @@
+#Lists ////////////////////////////
+mylist = ["apple","banana","cherry"]
+print(mylist," and type is :",type(mylist))
+
+mylist[0] = "lakshan"
+print(mylist)
+
+print(len(mylist))
+
+nums = [1,2,3,4,5,6]
+print(nums," and type is :",type(nums))
+
