f3521cf04c6fa8d7e98dc6f3e8d4ce4268de04ec
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2024-09-17 09:26:14 +0530

    add comment

--- cal.py
+++ cal.py
@@ -4,6 +4,8 @@ operator = input("Enter an operator(+ - /) :")
 num1 = float(input("Enter the 1st number : "))
 num2 = float(input("Enter the 2nd number : "))
 
+#using if and else
+
 if operator == "+":
     result = num1 + num2
     print(round(result,3))
