af65e747a01cb9a768fb71d16cbdbe2aedbd6d90
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2024-09-17 23:48:47 +0530

    Python_Practise_2.2

--- Python_Practise/two.py
+++ Python_Practise/two.py
@@ -10,3 +10,10 @@ print(len(mylist))
 nums = [1,2,3,4,5,6]
 print(nums," and type is :",type(nums))
 
+onelist = ["smith","nick","lakshan","ishan","karan","rajapaksha"]
+print(onelist[1:5])
+
+if "bhashitha" in onelist:
+    print("Yes 'bhashitha' is in the list")
+else:
+    print("No")
