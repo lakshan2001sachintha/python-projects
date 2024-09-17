cb62ba4b1b9e0f87f07793822795ac5a049acc9c
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2024-09-17 23:13:17 +0530

    adding more basics

--- Python_Practise/one.py
+++ Python_Practise/one.py
@@ -1,4 +1,4 @@
-#reminder python 
+#reminder python basics like datatypes 
 name = "lakshan"
 num = 34
 nam = 12.12
@@ -8,3 +8,18 @@ print(name)
 print(num)
 print(nam)
 print(lak)
+
+nam +=1
+print(nam)
+
+print(type(num))
+print(type(lak))
+
+lak = str("lakshan_sachintha_munasinghe")
+print(lak)
+
+nj = input(str)
+print("Your name is : ",nj)
+
+print(10<89)
+print(10==10)
