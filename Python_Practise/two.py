eed60e4ff784ece4dccaff35d9bc24c602c9a63e
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2024-09-18 00:17:59 +0530

    python_practise_2.3

--- Python_Practise/two.py
+++ Python_Practise/two.py
@@ -17,3 +17,36 @@ if "bhashitha" in onelist:
     print("Yes 'bhashitha' is in the list")
 else:
     print("No")
+
+print("-----------------------------")
+
+onelist[2:4] = ["vishmi","nethsara"]
+print(onelist)
+
+print("-----------------------------")
+
+onelist.insert(3,"kaveen")
+print(onelist)
+
+onelist.append("bananananan")
+print(onelist)
+
+print("-----------------------------")
+
+num1 = [1,2,3,4,5,6]
+num2 = [7,8,9,10,11,12]
+
+num1.extend(num2)
+print(num1)
+
+print("-----------------------")
+
+twolist = ["apple","banana","woodapple"]
+twolist.insert(2,"lakshan")
+print(twolist)
+
+for x in twolist:
+    print(x)
+
+for i in range(len(twolist)):
+    print(i)
