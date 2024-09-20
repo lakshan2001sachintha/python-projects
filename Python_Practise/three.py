d26ac49a646e6429f2eb14dfa653368e7bacbaf1
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2024-09-20 20:14:40 +0530

    practise02

--- Python_Practise/three.py
+++ Python_Practise/three.py
@@ -1,68 +1,86 @@
-a = 33
-b = 200
+#a = 33
+#b = 200
 
-if a>b:
-    print("a is greter than b")
-else :
-    print("b is greater than a")  
+#if a>b:
+ #   print("a is greter than b")
+#else :
+ #   print("b is greater than a")  
 
-print("------------------------")
+#print("------------------------")
 
-a = 33
-b = 200
+ # a = 33
+#b = 200
 
-if not a>b:
-    print("sachintha lakshan")
-else :
-    print("lakshan sachintha")    
+#if not a>b:
+    #print("sachintha lakshan")
+#else :
+    #print("lakshan sachintha")    
 
-print("----------LOOPS--------")
+#print("----------LOOPS--------")
 
-i = 1
-while i < 100:
-    i+=1
+#i = 1
+#while i < 100:
+    #i+=1
     
-    if(i%10==0):
-        continue
-    print(i)     
+    #if(i%10==0):
+       # continue
+    #print(i)     
 
-print("------------Functions----------------")       
-print("\n")
+#print("------------Functions----------------")       
+#print("\n")
 #def lakshan():
  #   print("My name is sachintha lakshan")
 
 #lakshan()    
 
-def my_func():
-    print("my name is lakshan sachintha munasinghe")
+#def my_func():
+   # print("my name is lakshan sachintha munasinghe")
 
-my_func()
-print("\n")
+#my_func()
+#print("\n")
 
-def my_lak(age =0,name ="enter your name !"):
-    print("My name is :",name," My age is :",age)
+#def my_lak(age =0,name ="enter your name !"):
+    #print("My name is :",name," My age is :",age)
 
-my_lak(23,"lakshan")
-my_lak()
+#my_lak(23,"lakshan")
+#my_lak()
 
-print("-----------------------")
+#print("-----------------------")
 
-def nam_func(names):
-    for x in names:
-        print(x)
+#def nam_func(names):
+    #for x in names:
+       # print(x)
 
-nam = ["lakshan","sachintha","munasinghe"]
-nam_func(nam)                 
+#nam = ["lakshan","sachintha","munasinghe"]
+#nam_func(nam)                 
 
-print("-----------------------------")
+#print("-----------------------------")
 
 #print("What is your age ?")
 #age = input()
 #print("your age is : ",age)
 
-print("lakshan"+"sachintha")
+#print("lakshan"+"sachintha")
 
-var = "123"
-print(type(var))
+#var = "123"
+#print(type(var))
 
-print(type(3.0))
+#print(type(3.0))
+
+#names = ["lakshan","sachintha","munasinghe","rajapaksha"]
+
+#for x in names:
+ #   print(x)
+
+#print("print fist 100 numbers in python by for loop")
+
+#for i in range(1,101):
+#    print(i)
+  
+arr1 = ["red","blue","yellow"]
+arr2 = ["apple","orange","cherry"]
+
+for x in arr1:
+    for y in arr2:
+        print(x,y)
+        
